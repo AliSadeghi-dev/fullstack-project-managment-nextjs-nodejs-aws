2c852a202eaa9cd013efa29293602e58de76ab75
Ali Sadeghi <alisadghi228@gmail.com>
2024-09-16 16:49:08 +0330

    feat : implement the backend and db

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -11,7 +11,8 @@ const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 //ROUTE IMPORTS
 const projectRoutes_1 = __importDefault(require("./routes/projectRoutes"));
-// COUNFIGURATION
+const taskRoutes_1 = __importDefault(require("./routes/taskRoutes"));
+// CONFIGURATION
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
     res.send("this is home route");
 });
 app.use("/projects", projectRoutes_1.default);
+app.use("/tasks", taskRoutes_1.default);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,8 +6,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 //ROUTE IMPORTS
 import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
 
-// COUNFIGURATION
+// CONFIGURATION
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/projects", projectRoutes);
+app.use("/tasks",taskRoutes)
 
 const port = process.env.PORT || 5000;
 
